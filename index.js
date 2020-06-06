27c492bf0942d3f961f07b6116a60222ac712582
jmpaniego <juanma_paniego@hotmail.com>
2020-06-05 23:21:31 -0300

    added nodemon dependency

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Saludos desde express');
+  res.send('Saludos express');
 });
 
 app.listen(3000, () => {
